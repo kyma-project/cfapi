23c5950480fa2489c0f2009f5aac86625d3a73fd
Zeort <samir.zeort@sap.com>
2024-10-21 12:29:42 +0300

    add scripts to generate serviceaccount based kubeconfig

---
+++ tools/kubeconfig/genkubeconfig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eo pipefail 
+set -o xtrace
+
+SCRIPTDIR=$(dirname "$0")
+
+kubectl apply -f $SCRIPTDIR/serviceaccount.yaml
+kubectl wait --for=jsonpath='{.data.token}' secret/admin-serviceaccount
+$SA_TOKEN=$(kubectl get secret admin-serviceaccount -o=go-template='{{.data.token | base64decode}}')
+cp ~/.kube/config kubeconfig-sa.yaml 
+docker run -v ${PWD}:/workdir -ti mikefarah/yq -i ".users |= [{\"name\":\"admin-serviceaccount\", \"user\": {\"token\":\"${SA_TOKEN}\"}}]" kubeconfig-sa.yaml
+docker run -v ${PWD}:/workdir -ti mikefarah/yq -i ".contexts[0].context.user |= \"admin-serviceaccount\"" kubeconfig-sa.yaml
