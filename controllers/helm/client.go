20c281ff54325f4cd96b0a01cc57a7333310b154
Danail Branekov <danailster@gmail.com>
2025-10-27 16:40:50 +0000

    Helm upgrade installable noop
    Currently helm installable make plenty of upgrades during reconcilation.
    This is fixed by cheking if values are different or the version only
    then upgrade is performed
    
    Co-authored-by: Yusmen Zabanov <yusmen.zabanov@sap.com>

--- controllers/helm/client.go
+++ controllers/helm/client.go
@@ -3,15 +3,17 @@ package helm
 import (
 	"context"
 	"fmt"
+	"reflect"
 
 	golog "log"
 
 	"github.com/go-logr/logr"
+	"gopkg.in/yaml.v3"
 	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/chart/loader"
 	"helm.sh/helm/v3/pkg/cli"
 	"helm.sh/helm/v3/pkg/release"
-	"helm.sh/helm/v3/pkg/storage/driver"
 )
 
 type HelmResult struct {
@@ -28,64 +30,99 @@ func NewClient() *Client {
 func (c *Client) Apply(ctx context.Context, chartPath string, releaseNamespace string, releaseName string, values map[string]any) (HelmResult, error) {
 	log := logr.FromContextOrDiscard(ctx).WithName("helm").WithValues("chart", releaseName)
 
-	releaseExists, releaseStatus, err := releaseExists(releaseNamespace, releaseName)
-	if err != nil {
-		return HelmResult{}, fmt.Errorf("failed to check if release %s exists in namespace %s: %w", releaseName, releaseNamespace, err)
-	}
-
-	if !releaseExists {
-		return c.install(ctx, chartPath, releaseNamespace, releaseName, values)
-	}
-
-	if releaseStatus.IsPending() {
-		log.Info("helm operation is pending", "releaseStatus", releaseStatus)
-		return HelmResult{
-			ReleaseStatus: releaseStatus,
-			Message:       "operation pending",
-		}, nil
-	}
-
-	return c.upgrade(ctx, chartPath, releaseNamespace, releaseName, values)
-}
-
-func releaseExists(namespace, name string) (bool, release.Status, error) {
-	actionConfig, err := newHelmActionConfig(namespace)
-	if err != nil {
-		return false, release.StatusUnknown, fmt.Errorf("failed to init helm action config: %w", err)
-	}
-
-	histClient := action.NewHistory(actionConfig)
-	histClient.Max = 1
-	versions, err := histClient.Run(name)
-
-	if err == driver.ErrReleaseNotFound {
-		return false, release.StatusUnknown, nil
-	}
-
-	if err != nil {
-		return false, release.StatusUnknown, fmt.Errorf("failed to check helm release hustory: %w", err)
-	}
-
-	if len(versions) == 0 {
-		return false, release.StatusUnknown, nil
-	}
-
-	lastVersion := versions[len(versions)-1]
-	if lastVersion.Info.Status == release.StatusUninstalled {
-		return false, lastVersion.Info.Status, nil
-	}
-
-	return true, lastVersion.Info.Status, nil
-}
-
-func (c *Client) install(ctx context.Context, chartPath string, releaseNamespace string, releaseName string, values map[string]any) (HelmResult, error) {
-	log := logr.FromContextOrDiscard(ctx).WithName("helm-install").WithValues("chart", releaseName)
-	log.Info("starting install")
-
 	chart, err := loader.Load(chartPath)
 	if err != nil {
 		return HelmResult{}, fmt.Errorf("failed to load chart at %s: %w", chartPath, err)
 	}
+
+	latestRelease, err := getLatestReleases(releaseNamespace, releaseName)
+	if err != nil {
+		log.Error(err, "failed to get latest release")
+		return HelmResult{}, fmt.Errorf("failed to get latest release %s in namespace %s: %w", releaseName, releaseNamespace, err)
+	}
+
+	if latestRelease == nil {
+		return c.install(ctx, chart, releaseNamespace, releaseName, values)
+	}
+
+	if latestRelease.Info.Status.IsPending() {
+		log.Info("helm operation is pending", "releaseStatus", latestRelease.Info.Status)
+		return HelmResult{
+			ReleaseStatus: latestRelease.Info.Status,
+			Message:       "operation pending",
+		}, nil
+	}
+
+	equalValues, err := equalValues(latestRelease.Config, values)
+	if err != nil {
+		return HelmResult{}, fmt.Errorf("failed to compare release values: %w", err)
+	}
+	equalVersions := chart.Metadata.Version == latestRelease.Chart.Metadata.Version
+
+	if equalValues && equalVersions {
+		log.Info("helm chart does not need update")
+		return HelmResult{
+			ReleaseStatus: latestRelease.Info.Status,
+		}, nil
+	}
+
+	return c.upgrade(ctx, chart, releaseNamespace, releaseName, values)
+}
+
+func equalValues(values1, values2 map[string]any) (bool, error) {
+	v1, err := marshalUnmarshal(values1)
+	if err != nil {
+		return false, fmt.Errorf("failed to marshal-unmarshal values1: %w", err)
+	}
+	v2, err := marshalUnmarshal(values2)
+	if err != nil {
+		return false, fmt.Errorf("failed to marshal-unmarshal values2: %w", err)
+	}
+
+	return reflect.DeepEqual(v1, v2), nil
+}
+
+func marshalUnmarshal(values map[string]any) (map[string]any, error) {
+	valuesBytes, err := yaml.Marshal(values)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal values: %w", err)
+	}
+
+	valuesUnamrshalled := make(map[string]any)
+	err = yaml.Unmarshal(valuesBytes, &valuesUnamrshalled)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal values: %w", err)
+	}
+
+	return valuesUnamrshalled, nil
+}
+
+func getLatestReleases(releaseNamespace string, releaseName string) (*release.Release, error) {
+	actionConfig, err := newHelmActionConfig(releaseNamespace)
+	if err != nil {
+		return nil, fmt.Errorf("failed to init helm action config: %w", err)
+	}
+
+	listClient := action.NewList(actionConfig)
+	listClient.Sort = action.ByDateDesc
+	listClient.Limit = 1
+	listClient.Filter = releaseName
+
+	versions, err := listClient.Run()
+	if err != nil {
+		return nil, fmt.Errorf("failed to list helm release %s%s: %w", releaseNamespace, releaseName, err)
+	}
+
+	if len(versions) == 0 {
+		return nil, nil
+	}
+
+	return versions[0], nil
+}
+
+func (c *Client) install(ctx context.Context, installedChart *chart.Chart, releaseNamespace string, releaseName string, values map[string]any) (HelmResult, error) {
+	log := logr.FromContextOrDiscard(ctx).WithName("helm-install").WithValues("chart", releaseName)
+	log.Info("starting install")
 
 	actionConfig, err := newHelmActionConfig(releaseNamespace)
 	if err != nil {
@@ -96,7 +133,7 @@ func (c *Client) install(ctx context.Context, chartPath string, releaseNamespace
 	installAction.Namespace = releaseNamespace
 	installAction.ReleaseName = releaseName
 
-	rel, err := installAction.Run(chart, values)
+	rel, err := installAction.Run(installedChart, values)
 	if err != nil {
 		return HelmResult{
 			ReleaseStatus: release.StatusUnknown,
@@ -109,14 +146,9 @@ func (c *Client) install(ctx context.Context, chartPath string, releaseNamespace
 	}, nil
 }
 
-func (c *Client) upgrade(ctx context.Context, chartPath string, releaseNamespace string, releaseName string, values map[string]any) (HelmResult, error) {
+func (c *Client) upgrade(ctx context.Context, upgradedChart *chart.Chart, releaseNamespace string, releaseName string, values map[string]any) (HelmResult, error) {
 	log := logr.FromContextOrDiscard(ctx).WithName("helm-upgrade").WithValues("chart", releaseName)
 	log.Info("starting upgrade")
-
-	chart, err := loader.Load(chartPath)
-	if err != nil {
-		return HelmResult{}, fmt.Errorf("failed to load chart at %s: %w", chartPath, err)
-	}
 
 	actionConfig, err := newHelmActionConfig(releaseNamespace)
 	if err != nil {
@@ -127,7 +159,7 @@ func (c *Client) upgrade(ctx context.Context, chartPath string, releaseNamespace
 	upgradeAction.Namespace = releaseNamespace
 	upgradeAction.Install = true
 
-	rel, err := upgradeAction.Run(releaseName, chart, values)
+	rel, err := upgradeAction.Run(releaseName, upgradedChart, values)
 	if err != nil {
 		return HelmResult{
 			ReleaseStatus: release.StatusUnknown,
